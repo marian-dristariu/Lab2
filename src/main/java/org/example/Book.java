a559fb2fffec197713c9ff0b8f568eb6bb5ec6cd
Dristariu Marian <125856274+marian-dristariu@users.noreply.github.com>
2023-10-10 13:48:13 +0300

    Initial commit

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Carte {
+    public String autor;
+    public String titlu;
+
+    
+}
---
+++ src/main/java/org/example/Library.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+    public ArrayList<Carte> booksList=new ArrayList<>();
+
+    public
+}
